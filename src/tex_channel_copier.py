9f7c35eda65f8fbc210312d8fe0f9fcd0f446e35
Tobias Soltvedt <tobsol01@gmail.com>
2023-09-18 22:14:45 +0200

    added matrix editing support

--- src/tex_channel_copier.py
+++ src/tex_channel_copier.py
@@ -3,38 +3,18 @@ from tex import Tex
 import numpy as np
 from PIL import Image
 
-def tex_channel_copier(directory: str, target : str, source : str):
+def tex_channel_copier(directory: str, array: np.array):
     '''copies the source channel of a given TEX file into the target channel, '''
-
-    if target not in channels.keys() or source not in channels.keys():
-        raise TypeError(f'{target} or {source} is not a valid channel, please use "r", "g", "b" or "a"')
 
     folder = Path(directory)
     # may need to change this check since naming doesnt necisarily have "--" in it
     grabber = list(folder.glob('**/*s.tex'))
 
     for file in grabber:
-        with open(file, "rb") as f:
-            new_bytes = bytearray(f.read())
-
         tex = Tex.from_file(file)
-        num_pixels = tex.hdr.height * tex.hdr.width
-        pixel_size = 4    # 4 bytes per pixel (32 bits)
-        
-        offset = 80
-        channels = {"b":0, "g":1, "r":2, "a":3}
-
-        # Copy the alpha channel into the blue channel for each pixel
-        try:
-            for _ in range(tex.hdr.mip_levels):
-                for i in range(offset, offset:= offset + num_pixels * pixel_size, pixel_size):
-                    new_bytes[i + channels[target]] = new_bytes[i + channels[source]]
-                num_pixels = num_pixels//4 
-
-            with open(file, "wb") as f:
-                f.write(new_bytes)
-        except:
-            print(file)
+        array = np.array([[0,0,0,0], [0,1,0,0], [0,0,1,0], [1,0,0,1]])
+        tex.bdy.from_bytes(array)
+        tex._write()
 
 def get_tex(file: str):
     return Tex.from_file(file)
@@ -51,37 +31,35 @@ def get_image(data: np.array, height: int, width: int):
 
     return Image.fromarray(image, mode="RGBA")
 
-def copy_channel(file: str, target : str, source : str):
-    tex = Tex.from_file(file)
-    pixel_size = 4    # 4 bytes per pixel (32 bits)
+def byte_to_int(byte):
+    return int.from_bytes(byte, byteorder='big')
+
+def int_to_byte(number):
+    max_byte, min_byte = 255, 0
+    return int.to_bytes(max(min(int(number), max_byte), min_byte))
+
+def copy_channel(tex: Tex, modification_matrix : np.array):
+    data = get_data(tex)
+
+    vectorized_byte_to_int = np.vectorize(byte_to_int)
+    int_array = vectorized_byte_to_int(data)
+
+    modified_array = np.matmul(int_array, modification_matrix)
+
+    vectorized_int_to_byte = np.vectorize(int_to_byte)
+    byte_array = b''.join(vectorized_int_to_byte(modified_array).reshape(-1))
+
+    return byte_array
+
     
-    channels = {"b":0, "g":1, "r":2, "a":3}
-
-    header = tex.hdr.data
-    print(len(header))
-    
-    '''
-        np.array(contents).reshape((len(contents)//pixel_size, pixel_size))
-
-    # Copy the alpha channel into the blue channel for each pixel
-    try:
-        for _ in range(tex.hdr.mip_levels):
-            for i in range(offset, offset:= offset + num_pixels * pixel_size, pixel_size):
-                new_bytes[i + channels[target]] = new_bytes[i + channels[source]]
-            num_pixels = num_pixels//4 
-
-        with open(file, "wb") as f:
-            f.write(new_bytes)
-    except:
-        print(file)
-    '''
 
 if __name__ == "__main__":
-    copy_channel("src/data/target.tex", "b", "r")
+    array = np.array([[1,0,0,0], [0,2,0,0], [0,0,1,0], [0,0,0,1]])
     tex = get_tex("src/data/target.tex")
-    data = get_data(tex)
-    image = get_image(data, tex.hdr.height, tex.hdr.width)
-    image.show()
+    copy_channel(tex, array)
+    #data = get_data(tex)
+    #image = get_image(data, tex.hdr.height, tex.hdr.width)
+    #image.show()
 
 
 
